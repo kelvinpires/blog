92b9d814afeca22f8c0c3ca7b691cc73c5712cf8
Kelvin Pires <pireskelvin7@gmail.com>
2022-04-25 12:25:56 -0300

    Change console.log

--- api/src/connectDb.js
+++ api/src/connectDb.js
@@ -9,7 +9,7 @@ function connectDb() {
   const db = mongoose.connection;
 
   db.on("error", (err) => console.log(err));
-  db.once("open", () => console.log("Database is online"));
+  db.once("open", () => console.log("Connected to the database"));
 }
 
 module.exports = { connectDb };
