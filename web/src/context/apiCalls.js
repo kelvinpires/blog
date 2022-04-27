3656a472725fafe556cddd690e14d844b211b677
Kelvin Pires <pireskelvin7@gmail.com>
2022-04-26 21:48:13 -0300

    Create apiCalls

--- web/src/context/apiCalls.js
+++ web/src/context/apiCalls.js
@@ -4,9 +4,16 @@ const baseUrl = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-export const getAllPosts = async () => {
+export const getAllPosts = async (setPosts) => {
   const res = await baseUrl.get("/posts");
   const data = await res.data.posts;
 
-  console.log(data);
+  setPosts(data);
+};
+
+export const getPost = async (postId, setPost) => {
+  const res = await baseUrl.get(`/posts/${postId}`);
+  const data = await res.data.post;
+
+  setPost(data);
 };
