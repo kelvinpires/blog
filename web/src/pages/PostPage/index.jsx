1539b77e25acd6af7a784275c5bc2a89b2ee920f
Kelvin Pires <pireskelvin7@gmail.com>
2022-05-21 20:34:01 -0300

    Create Post page

---
+++ web/src/pages/PostPage/index.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { PostData, PostTitle } from "../../components/HomePosts/styles";
+import { getPost } from "../../context/PostContext/apiCalls";
+import {
+  PostPageContainer,
+  PostPageContent,
+  PostPageImageContainer,
+  PostPageImg,
+  PostText,
+  PostDataContent,
+} from "./styles";
+
+export const PostPage = () => {
+  const [post, setPost] = useState([]);
+  const { postId } = useParams();
+
+  useEffect(() => {
+    getPost(postId, setPost);
+    scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  const postData =
+    post != false ? new Date(post.createdAt).toLocaleDateString() : null;
+  const postDataUpdated = post
+    ? new Date(post.updatedAt).toLocaleDateString()
+    : null;
+
+  return (
+    <>
+      {post != false && (
+        <>
+          <PostPageContainer>
+            <PostPageContent style={{ alignItems: "flex-start" }}>
+              <PostTitle style={{ fontSize: "5rem" }}>{post.title}</PostTitle>
+              <PostDataContent>
+                <PostData style={{ fontSize: "2rem" }}>
+                  Criado em {postData}
+                </PostData>
+                <PostData style={{ fontSize: "2rem" }}>
+                  Atualizado em {postDataUpdated}
+                </PostData>
+              </PostDataContent>
+            </PostPageContent>
+
+            <PostPageContent style={{ gap: "4rem" }}>
+              <PostPageImageContainer>
+                <PostPageImg
+                  src={post.image}
+                  alt={post.title}
+                  title={post.title}
+                />
+              </PostPageImageContainer>
+              <PostText>{post.text}</PostText>
+            </PostPageContent>
+          </PostPageContainer>
+        </>
+      )}
+    </>
+  );
+};
---
+++ web/src/pages/PostPage/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const PostPageContainer = styled.main`
+  padding: 6rem 25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  background-color: var(--light-purple);
+`;
+
+export const PostPageContent = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+`;
+
+export const PostPageImageContainer = styled.div`
+  width: 100%;
+  height: auto;
+  border-radius: 0.8rem;
+`;
+
+export const PostPageImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 0.8rem;
+`;
+
+export const PostDataContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5rem;
+`;
+
+export const PostText = styled.p`
+  font-size: 2rem;
+  color: var(--text-color);
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  line-height: 1.6;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
